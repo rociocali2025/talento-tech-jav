fa792c8e3c6347a4bb518ed8550a570ecd7fb5ba
Rociocali2025 <delcet@hotmail.com>
2025-08-13 18:20:49 -0500

    correcion de archivo

--- DOM/main.js
+++ DOM/main.js
@@ -1,45 +1,43 @@
-const v1= "tio ben";
-const v2= "recuerda lo siguiente";
-const v3= "un gran poder requiere una gran responsabilidad";
-const v4= "ten cuidado con el duende verde";
+const v1 = "tio ben";
+const v2 = "recuerda lo siguiente";
+const v3 = "un gran poder requiere una gran responsabilidad";
+const v4 = "ten cuidado con el duende verde";
 
-const spiderman =(peligro)=>{
-    if (peligro == true){
+const spiderman = (peligro) => {
+    if (peligro == true) {
         return `como decia el ${v1} ${v2} ${v3}`;
-    }else{
+    } else {
         return `como decia el ${v1} ${v4}`;
     }
 };
- 
 
-const titulo = documento.createElement("h1");
-titulo.textContent ="hola me llamo spiderman"
+// Crear título
+const titulo = document.createElement("h1");
+titulo.textContent = "hola me llamo spiderman";
 document.body.appendChild(titulo);
 
-// crear botones
-const boton= document.createElement("button");
-const boton2= document.createElement("button");
+// Crear botones
+const boton = document.createElement("button");
+const boton2 = document.createElement("button");
 
-boton.innertext="peligro";
-boton2.innertext="No peligro";
+boton.innerText = "peligro";
+boton2.innerText = "No peligro";
 
-// Añadir botones a la pagina
+// Añadir botones a la página
 document.body.appendChild(boton);
 document.body.appendChild(boton2);
 
-
 // Eventos
-boton.addEventListener("click",()=>{
-    const peligro= true;
-    const recordando= spiderman(peligro);
-    windows.location.href="index.html";
-    document.body.innerHTML=`<h1>${recordando}</h1>
-    <button id ='boton2'><a href='index.html'>volver</a></button>`;
-    
-    boton2,addEventListener
+boton.addEventListener("click", () => {
+    const peligro = true;
+    const recordando = spiderman(peligro);
+    document.body.innerHTML = `<h1>${recordando}</h1>
+    <a href='index.html'><button>volver</button></a>`;
+});
 
-})
-
-
-
-
+boton2.addEventListener("click", () => {
+    const peligro = false;
+    const recordando = spiderman(peligro);
+    document.body.innerHTML = `<h1>${recordando}</h1>
+    <a href='index.html'><button>volver</button></a>`;
+});
